b99d975a4fa37a0411ca2473b5211f6431b1c6a0
botato2048 <ema@tfs.ca>
2023-04-28 08:59:06 -0400

    Initial Commit

---
+++ SwiftAuthentication/SwiftAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAuthenticationApp.swift
+//  SwiftAuthentication
+//
+//  Created by em on 2023-04-28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
